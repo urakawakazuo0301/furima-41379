6edb6c458ebeb63546dd4e05585b31993ba8978c
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-15 15:09:13 +0900

    ユーザー認証機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+ 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width characters' } do
+  validates :last_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/u, message: 'is invalid. Input full-width katakana characters' } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
+
 
   has_many :items
   has_many :orders
+
+  
 
 end
