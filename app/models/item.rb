d179b148f1e88edfdf0da34d02df442193e34442
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 11:31:35 +0900

    item_conditionモデル定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,13 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
 
 
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
-  validates :item_condition_id, presence: true
+  validates :item_condition_id, numericality: { other_than: 1 , message: "Item Condition can't be blank"}
   validates :shipping_cost_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_date_id, presence: true
