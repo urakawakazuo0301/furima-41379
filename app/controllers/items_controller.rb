fb408b04eebd30bc839947cda60513450f62c653
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 10:13:19 +0900

    ストロングパラメーターの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,12 @@ class ItemsController < ApplicationController
   end
 
   
+  
+
+  private
+
+  def items_params
+    params.require(:item).permit(:name, :description, :category_id, :item_condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
