ed66bdd50dda9f15211feb8af98cbb00dce081b0
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 10:40:20 +0900

    itemモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true
