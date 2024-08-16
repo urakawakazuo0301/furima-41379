3a90e1b0bfdae37351e5033761d879475f10c9ae
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 17:15:21 +0900

    手数料・利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+const price = () => {
+
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener(`input`, () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
