4663c1f7135a1d48a56e55a5c0ea13ef3811836e
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 11:40:38 +0900

    shipping_costモデル定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,14 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :item_condition
+  belongs_to :shipping_cost
 
 
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "Category can't be blank"}
   validates :item_condition_id, numericality: { other_than: 1 , message: "Item Condition can't be blank"}
-  validates :shipping_cost_id, presence: true
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "Shipping Cost can't be blank"}
   validates :prefecture_id, presence: true
   validates :shipping_date_id, presence: true
   validates :price, presence: true
