863ecf911d4272105ea5e1d329500f2f03ad8214
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-16 09:42:25 +0900

    itemモデルにバリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :shipping_cost_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :price, presence: true
+  
 end
