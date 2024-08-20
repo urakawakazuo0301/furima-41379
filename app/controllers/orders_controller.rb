310fa162001ccac3eabe4ac71714ae407c1b0796
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-20 12:07:45 +0900

    privateメソッド設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,7 @@ class OrdersController < ApplicationController
     end
   end
 
+  private
 
   def order_params
     params.require(:buyer_order).permit(:postcode, :prefecrute_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: item_id)
