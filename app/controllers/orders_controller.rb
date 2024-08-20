40c967e29f74675836f209e391ccc001f04b2054
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-20 12:06:52 +0900

    Formオブジェクトのインスタンス生成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,22 @@
 class OrdersController < ApplicationController
 
-def index
-end
+  def index
+    @buyer_order = BuyerOrder.new
+  end
 
+  def create
+    @buyer_order = BuyerOrder.new(order_params)
+    if @buyer_order.valid?
+      @buyer_order.save
+      redirect_to '/'
+    else
+      render "/", status: unprocessable_entity
+    end
+  end
+
+
+  def order_params
+    params.require(:buyer_order).permit(:postcode, :prefecrute_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+  end
 
 end
