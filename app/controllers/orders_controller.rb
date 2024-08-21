db471d576bdc735d0361ed6cf2c2fc09b9203042
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-21 17:43:57 +0900

    コントローラーに決済処理の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,35 @@
 class OrdersController < ApplicationController
-  
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @orderform = OrderForm.new
   end
 
   def create
     @orderform = OrderForm.new(order_params)
     if @orderform.valid?
+      pay_item
       @orderform.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_e8e9ddb14878b2c4b431f506"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
 
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
