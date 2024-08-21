dc6754bdb54a4a32f3fd37da4ee46f56cdd7608f
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-21 17:47:03 +0900

    tokenのバリデーション設定

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -10,6 +10,7 @@ class OrderForm
     validates :phone_number, format: {with: /\A\d{11}\z/, message:"is invalid. Input only number"}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
