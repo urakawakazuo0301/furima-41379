88ddb7540a21b487265110b045fa88d3842e000e
urakawakazuo0301 <urakawakazuo0301@gmail.com>
2024-08-22 12:01:05 +0900

    order_formバリデーション変更

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -7,7 +7,7 @@ class OrderForm
     validates :city
     validates :block
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-    validates :phone_number, format: {with: /\A\d{11}\z/, message:"is invalid. Input only number"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message:"is invalid. Input only number"}
     validates :user_id
     validates :item_id
     validates :token
